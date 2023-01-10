a96fd9846d6904e4868b02e4de8dc3f14056ca68
flyingRKO <wogns970@naver.com>
2023-01-10 18:09:25 +0900

    Setup Spring Boot 3.0.1 + java 17 기본 구성은 web + actuator + lombok 우선 이정도만

---
+++ src/main/java/com/rko/numberofusersstatusboard/NumberOfUsersStatusBoardApplication.java
@@ -0,0 +1,13 @@
+package com.rko.numberofusersstatusboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberOfUsersStatusBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumberOfUsersStatusBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rko/numberofusersstatusboard/NumberOfUsersStatusBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rko.numberofusersstatusboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberOfUsersStatusBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
