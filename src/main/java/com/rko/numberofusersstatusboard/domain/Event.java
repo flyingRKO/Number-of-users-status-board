bebac5939540267ac67b3e0b55783b714b319454
Lee Jae Hun <wogns970@naver.com>
2023-01-12 16:37:31 +0900

    #1 - 도메인 objects and enums
    개발하면서 기능 입출력에 적용해보기 위해
    필요한 도메인 오브젝트를 설계
    추후 JPA를 사용할 때 참고하거나 활용할 예정

---
+++ src/main/java/com/rko/numberofusersstatusboard/domain/Event.java
@@ -0,0 +1,23 @@
+package com.rko.numberofusersstatusboard.domain;
+
+import com.rko.numberofusersstatusboard.constant.EventStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Event {
+    private Long id;
+
+    private Long placeId;
+    private String eventName;
+    private EventStatus eventStatus;
+    private LocalDateTime eventStartDatetime;
+    private LocalDateTime eventEndDatetime;
+    private Integer currentNumberOfPeople;
+    private Integer capacity;
+    private String memo;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+}
